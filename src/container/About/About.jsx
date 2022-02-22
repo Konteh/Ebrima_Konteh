59d9cebede13453bcedf3493714a97ee893ae460
Konteh <kontehebrima993@gmail.com>
2022-02-22 04:09:57 -0800

    connect sanity to the frontend react

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -3,28 +3,19 @@ import { motion } from 'framer-motion';
 
 // import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss';
-import {images} from '../../constants'
-// import { urlFor, client } from '../../client';
-
-// const About = () => {
-//   const [abouts, setAbouts] = useState([]);
-
-//   useEffect(() => {
-//     const query = '*[_type == "abouts"]';
-
-//     client.fetch(query).then((data) => {
-//       setAbouts(data);
-//     });
-//   }, []);
-
-const abouts = [
-    { title: 'web developent', description: 'Am good at it', imgUrl:images.about01 },
-    { title: 'web Design', description: 'Am good at it', imgUrl:images.about02 },
-    { title: 'UI/Ux', description: 'Am good at it', imgUrl:images.about03},
-    { title: 'Web Animation', description: 'Am good at it', imgUrl:images.about04},
-]
+import { urlFor, client } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
       <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
@@ -38,7 +29,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
@@ -46,7 +37,6 @@ const About = () => {
       </div>
     </>
   );
-        }
-
+};
 
 export default About
