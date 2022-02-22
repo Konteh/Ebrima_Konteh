f9485629f94eaef1b9920464bebe557181972678
Konteh <kontehebrima993@gmail.com>
2022-02-22 05:17:25 -0800

    create the skills and experience section

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -108,10 +108,10 @@ const Work = () => {
   );
 };
 
-export default AppWrap(Work, 'work');
 
-// export default AppWrap(
-//   MotionWrap(Work, 'app__works'),
-//   'work',
-//   'app__primarybg',
-// );
+
+export default AppWrap(
+  MotionWrap(Work, 'app__works'),
+  'work',
+  'app__primarybg',
+);
